186b26c996d8299d43b6384040bc404ea079914d
Julian David Nuñez Franco <jdnunez@utap.edu.co>
2026-09-21 21:42:20 -0500

    Se cambia logo y css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Logo from '../../logo.png'
-
-import './Navbar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../logo.png";
+import "./Navbar.css";
 
 export default function NavBar() {
-    return (
-        <header className="navbar">
-           <div>
-          <NavLink exact to="/" >
-                <img id="logo" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-            </NavLink>    
-            </div>
-            <nav>
-                <ul className="list">
-                    <li className="list-item">
-                        
-                    </li>
-                    <li className="list-item">
-                    
-                    </li>
-                </ul>
-          </nav>
-        </header>
-    )
+  return (
+    <header className="navbar">
+      <div>
+        <NavLink exact to="/">
+          <img
+            id="logo"
+            src={Logo}
+            width="auto"
+            height="auto"
+            className="d-inline-block align-top"
+            alt="Redux Blog"
+          />
+        </NavLink>
+      </div>
+
+      <h4>Bienvenidos a mi blog</h4>
+      <nav>
+        <ul className="list">
+          <li className="list-item">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/filter/posts">Posts</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
